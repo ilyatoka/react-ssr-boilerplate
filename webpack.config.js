085c76040a7aea018616ebbb9fc121651d68b346
Ilya Toka <ilyatoka.gm@gmail.com>
2019-01-08 16:11:55 +0200

    Add webpack.config.js config file

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const miniCssExtractPlugin = new MiniCssExtractPlugin({
+  filename: "[name].[hash].css",
+  chunkFilename: "[id].[hash].css"
+});
+
+const cleanWebpackPlugin = new CleanWebpackPlugin(["build", "dist"]);
+
+module.exports = {
+  mode: "development",
+
+  entry: {
+    main: "./src/index.jsx"
+  },
+
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].[hash].js"
+  },
+
+  resolve: {
+    modules: ["node_modules", "src"],
+    extensions: [".js", ".jsx"]
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              camelCase: true,
+              localIdentName: "[name]__[local]__[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
+
+  plugins: [miniCssExtractPlugin, cleanWebpackPlugin]
+};
