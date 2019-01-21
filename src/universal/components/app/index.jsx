a7852ef5638b07fee5299a15242d5b4d5e2b8efb
Ilya Toka <ilyatoka.gm@gmail.com>
2019-01-21 13:43:59 +0200

    Connect app component to the redux-store

--- src/universal/components/app/index.jsx
+++ src/universal/components/app/index.jsx
@@ -2,6 +2,7 @@ import styles from "./app.scss";
 import React from "react";
 import PropTypes from "prop-types";
 import { Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 import MainNav from "universal/components/main-nav";
 import HomePage from "universal/components/home-page";
 import PageA from "universal/components/page-a";
@@ -11,17 +12,27 @@ const propTypes = {};
 
 const defaultProps = {};
 
-export default function App(props) {
-  return (
-    <React.Fragment>
-      <MainNav />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/page-a" component={PageA} />
-        <Route path="/page-b" component={PageB} />
-      </Switch>
-    </React.Fragment>
-  );
+@connect((state, props) => {
+  return {
+    test: state.getIn(["app", "test"])
+  };
+})
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <MainNav />
+        <h3 className={styles.title}>
+          Test server-rendered response from the api: <b>{this.props.test}</b>
+        </h3>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/page-a" component={PageA} />
+          <Route path="/page-b" component={PageB} />
+        </Switch>
+      </React.Fragment>
+    );
+  }
 }
 
 App.propTypes = propTypes;
