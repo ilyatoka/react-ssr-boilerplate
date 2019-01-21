15252d9bb56342c5c17ef4c892e35984af535eb5
Ilya Toka <ilyatoka.gm@gmail.com>
2019-01-21 13:46:38 +0200

    Update createStore with saga-middleware and immutable state

--- src/universal/store/store.js
+++ src/universal/store/store.js
@@ -1,8 +1,11 @@
+import Immutable, { fromJS } from "immutable";
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { createBrowserHistory, createMemoryHistory } from "history";
-import { routerMiddleware } from "connected-react-router";
-import createRootReducer from "./reducers";
+import { routerMiddleware } from "connected-react-router/immutable";
+import createSagaMiddleware, { END } from "redux-saga";
+import { createRootReducer } from "./reducers";
+import { rootSaga } from "./sagas";
 import { isServer } from "universal/utils/helpers";
 
 export default (preloadedState, url = "/") => {
@@ -12,14 +15,32 @@ export default (preloadedState, url = "/") => {
       })
     : createBrowserHistory();
 
+  const immutablePreloadedState = fromJS(preloadedState);
+
   const rootReducer = createRootReducer(history);
-  const middlewares = [routerMiddleware(history)];
+  const sagaMiddleware = createSagaMiddleware(rootSaga);
+  const connectedRouterMiddleware = routerMiddleware(history);
+
+  const middlewares = [sagaMiddleware, connectedRouterMiddleware];
   const middlewareEnhancer = applyMiddleware(...middlewares);
 
   const enhancers = [middlewareEnhancer];
-  const composedEnhancers = composeWithDevTools(...enhancers);
+  const composeEnhancers = composeWithDevTools({
+    serialize: { immutable: Immutable }
+  });
 
-  const store = createStore(rootReducer, preloadedState, composedEnhancers);
+  const store = createStore(
+    rootReducer,
+    immutablePreloadedState,
+    composeEnhancers(...enhancers)
+  );
+
+  if (isServer) {
+    store.serverRunSaga = () => sagaMiddleware.run(rootSaga);
+    store.serverStopSaga = () => store.dispatch(END);
+  } else {
+    sagaMiddleware.run(rootSaga);
+  }
 
   return { store, history };
 };
