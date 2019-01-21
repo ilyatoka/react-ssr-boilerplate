eefadadafc62f123678ce38977313ec7e1cb969a
Ilya Toka <ilyatoka.gm@gmail.com>
2019-01-21 13:44:13 +0200

    Add isDevMode helper

--- src/universal/utils/helpers.js
+++ src/universal/utils/helpers.js
@@ -1,3 +1,13 @@
+export const isDevMode =
+  (typeof window !== "undefined" &&
+    window &&
+    window.location &&
+    window.location.hostname) === "localhost" ||
+  (typeof window !== "undefined" &&
+    window &&
+    window.location &&
+    window.location.hostname) === "127.0.0.1";
+
 export const isServer = !(
   typeof window !== "undefined" &&
   window.document &&
